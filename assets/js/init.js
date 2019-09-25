50584a222a24c98da2b15f4bf03c1d0d45a14278
rf <rf.fa17@gmail.com>
2019-09-25 20:06:52 +0330

    a simple materialized template replaced minima and head.html and navbar.html was modified

---
+++ assets/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav({
+      edge: 'right',
+      draggable: true,
+    });
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
